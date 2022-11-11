b47df006207392704c176efca081421169334380
esratncr <tuncer.esra1453@gmail.com>
2022-11-11 13:54:58 +0300

    --

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./List.css";
+import Search from "./Search";
+
+
+
+
 
 const Header = (props) => {
   
   return (
     <nav className="navbar  ">
+  
       <div className="container-fluid ">
         <Link className=" navbar-brand text-danger " to="/">
           Kitaplığım
         </Link>
+        
+        <Search />
+    
 
+
+
+      
       </div>
     </nav>
   );
